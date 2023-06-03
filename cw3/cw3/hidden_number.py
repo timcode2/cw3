bd29ac287845eb8ef2c81d90da145af1ed8dc814
artemm <artem.m2013@gmail.com>
2023-06-03 16:42:31 +0300

    upgrade hidden_number.py

--- cw3/cw3/hidden_number.py
+++ cw3/cw3/hidden_number.py
@@ -5,8 +5,19 @@ def format_from_account(input_from_account):
     :return: Скрытый счёт отправителя
     '''
     from_account_split = input_from_account.split(" ")
-    new_str = ' ' + from_account_split [-1][0:4] + ' ' + from_account_split [-1][4:6] + '** ' + '**** ' + from_account_split [-1][12:16]
-    return from_account_split[0] + new_str
+    if len(from_account_split) == 2:
+        if len(from_account_split[-1]) == 16:
+            new_str = ' ' + from_account_split[-1][0:4] + ' ' + from_account_split[-1][4:6] + '** ' + '**** ' + from_account_split[-1][12:16]
+        elif len(from_account_split[-1]) == 20:
+            new_str = ' ' + from_account_split[-1][0:4] + ' ' + from_account_split[-1][4:6] + '** ' + '**** ' + from_account_split[-1][16:20]
+        return from_account_split[0] + new_str
+
+    if len(from_account_split) == 3:
+        if len(from_account_split[-1]) == 16:
+            new_str = ' ' + from_account_split[-1][0:4] + ' ' + from_account_split[-1][4:6] + '** ' + '**** ' + from_account_split[-1][12:16]
+        elif len(from_account_split[-1]) == 20:
+            new_str = ' ' + from_account_split[-1][0:4] + ' ' + from_account_split[-1][4:6] + '** ' + '**** ' + from_account_split[-1][16:20]
+        return from_account_split[0] + ' ' + from_account_split[1] + new_str
 
 
 def format_to_account(input_to_account):
@@ -16,5 +27,15 @@ def format_to_account(input_to_account):
     :return: Скрытый счёт получателя
     """
     to_account_split = input_to_account.split(" ")
-    new_str = ' **' + to_account_split[1][16:20]
-    return to_account_split[0] + new_str
+    if len(to_account_split) == 2:
+        if to_account_split[-1] == 20:
+            new_str = ' **' + to_account_split[-1][16:20]
+        else:
+            new_str = ' **' + to_account_split[-1][12:16]
+        return to_account_split[0] + new_str
+    elif len(to_account_split) == 3:
+        if to_account_split[-1] == 20:
+            new_str = ' **' + to_account_split[-1][16:20]
+        else:
+            new_str = ' **' + to_account_split[-1][12:16]
+        return to_account_split[0] + ' ' + to_account_split[1] + new_str
