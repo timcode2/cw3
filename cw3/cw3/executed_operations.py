fcba838642896682d102555ed93947f881367014
artemm <artem.m2013@gmail.com>
2023-06-03 07:26:33 +0300

    upgrade executed_operations.py

--- cw3/cw3/executed_operations.py
+++ cw3/cw3/executed_operations.py
@@ -8,7 +8,32 @@ def get_executed_operations(operation_list):
     for operation in operation_list:
         if 'state' in operation.keys():
             if operation['state'] == 'EXECUTED':
-                executed_operations.append(operation)
-        else:
-            continue
-    return executed_operations[0:5]
+                date = operation.get('date')
+                formatted_date = date[:10]
+                executed_operations.append((formatted_date, operation))
+            else:
+                continue
+
+    sorted_operations = sorted(executed_operations, key=lambda x: x[0], reverse=True)
+    last_five_operations = [operation[1] for operation in sorted_operations[:5]]
+    print(last_five_operations)
+    return last_five_operations
+
+# def get_executed_operations(operation_list):
+#     """
+#     Функция игнорирует операции статус перевода которых неизвестен
+#     :param operation_list: Список со словарями с данными по операциям
+#     :return: Список из 5 успешных операций
+#     """
+#
+#     formatted_operations = []
+#     for operation in operation_list:
+#         if operation.get('state') == 'EXECUTED':
+#             date = operation.get('date')
+#             formatted_date = date[:10]
+#             formatted_operations.append((formatted_date, operation))
+#
+#     sorted_operations = sorted(formatted_operations, key=lambda x: x[0], reverse=True)
+#     last_five_operations = [operation[1] for operation in sorted_operations[:5]]
+#     print(last_five_operations)
+#     return last_five_operations
